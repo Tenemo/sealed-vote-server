528bbfd5cb62481f92ba99c468ca186f7f9a37c8
Piotr Piech <tenemo@gmail.com>
2022-04-21 04:48:14 +0200

    initial configuration

---
+++ .babelrc.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const extensions = ['.js', '.ts'];
+const { baseUrl } = JSON.parse(
+    fs.readFileSync('./tsconfig.json', 'utf8'),
+).compilerOptions;
+
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: "node 16.14",
+                useBuiltIns: 'usage',
+                corejs: 3,
+                modules: 'commonjs',
+            },
+        ],
+        '@babel/preset-typescript',
+    ],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                extensions,
+                root: [baseUrl],
+            },
+        ],
+        '@babel/plugin-syntax-top-level-await',
+    ],
+    ignore: ['node_modules'],
+};
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from 'fastify';
+
+const PORT = process.env.PORT || 4000;
+
+const start = async (): Promise<void> => {
+    const fastify = Fastify();
+
+    fastify.get('/health', async () => {
+        return { hello: 'world' };
+        // await new Promise((res) => res(123));
+    });
+
+    try {
+        await fastify.listen(PORT);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+start().catch((error) => console.error(error));
